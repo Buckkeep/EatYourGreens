4f6e169f3642efe462d7294b3cd1e25430b9cf24
Buckkeep <neeta.b+github@gmail.com>
2023-07-10 18:58:32 -0400

    Header and first line added (Basic banner)

---
+++ eatyourgreens/src/components/Banner.js
@@ -0,0 +1,10 @@
+function Banner() {
+    return (
+        <div>
+            <h1>Eat Your Greens!</h1>
+            <p>Select what you've eaten this week:</p>
+        </div>
+    )
+}
+
+export default Banner
